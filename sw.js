70ce4a1f2d6f7d1e91eb091d023a9ced1eb7c974
Vicente Montañana Montañana <78200991+vmontana@users.noreply.github.com>
2026-05-14 16:25:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'v1';
+const CACHE = 'v2';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
